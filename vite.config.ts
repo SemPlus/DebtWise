c47674977f5b44fdd88507c8f87dde2dc603e657
Sem <sem.gk01@gmail.com>
2025-12-21 18:52:20 +0100

    Fix deployment action and blank screen paths

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig(({ mode }) => {
       react(),
       VitePWA({
         registerType: 'autoUpdate',
-        includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+        includeAssets: ['icons/icon-192.png', 'icons/icon-512.png'],
         manifest: {
           name: 'DebtWise - Smart Debt Tracker',
           short_name: 'DebtWise',
